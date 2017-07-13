be19fa89bb51df564dff7b127e0a07e07a34710a
dankato <apearmor@gmail.com>
2017-07-13 09:37:30 -0700

    creating user, first 3 bullets complete

--- models.js
+++ models.js
@@ -1,4 +1,39 @@
+'use strict';
+
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+// add schema
+const userSchema = mongoose.Schema({
+  username: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  firstName: {type: String, default: ''},
+  lastName: {type: String, default: ''}
+});
+
+// add hash password (with salt factor of 10)
+userSchema.statics.hashPassword = function(password) {
+  return bcrypt
+    .hash(password, 10)
+    .then(hash => hash);
+};
+
+// add validate password
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt
+    .compare(password, this.password)
+    .then(isValid => isValid);
+};
+
+// add apiRepr method
+userSchema.methods.apiRepr = function() {
+  return {
+    username: this.username || '',
+    firstName: this.firstName || '',
+    lastName: this.lastName || ''
+  };
+};
+
 
 const blogPostSchema = mongoose.Schema({
   author: {
@@ -9,7 +44,6 @@ const blogPostSchema = mongoose.Schema({
   content: {type: String},
   created: {type: Date, default: Date.now}
 });
-
 
 blogPostSchema.virtual('authorName').get(function() {
   return `${this.author.firstName} ${this.author.lastName}`.trim();
@@ -23,8 +57,9 @@ blogPostSchema.methods.apiRepr = function() {
     title: this.title,
     created: this.created
   };
-}
+};
 
+const User = mongoose.model('User', userSchema);
 const BlogPost = mongoose.model('BlogPost', blogPostSchema);
 
-module.exports = {BlogPost};
+module.exports = {User, BlogPost};
